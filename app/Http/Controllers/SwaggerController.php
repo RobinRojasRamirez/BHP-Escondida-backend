9583043b0ccd416c6d259cfb9bfc7be36739dde1
Robin Rojas <robinrojasramirez12@gmail.com>
2025-03-02 18:53:05 -0500

    configuracion inicial backend

---
+++ app/Http/Controllers/SwaggerController.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class SwaggerController extends Controller
+{
+    // Este controlador solo contiene la configuración de Swagger y no necesita métodos.
+}
